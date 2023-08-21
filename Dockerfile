c88ee82786afca2310a7a1f8118acb4a8ce389b9
ThisisYoYoDev <yoel.edery@epitech.eu>
2023-08-21 17:59:17 +0200

    feat(ffa): docker version
    Signed-off-by: ThisisYoYoDev <yoel.edery@epitech.eu>

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,11 @@
 FROM python:3.11.4-slim
 
+WORKDIR /app
+
+COPY requirements.txt requirements.txt
+
+RUN pip install --user -r requirements.txt
+
+COPY . .
+
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from src.endpoint import router
 app = FastAPI()
 
 app.include_router(router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
