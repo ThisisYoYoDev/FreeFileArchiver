03f3f28c53fcbaa03becea14b41d97d2040f94f7
ThisisYoYoDev <yoel.edery@epitech.eu>
2023-11-13 10:30:50 -0800

    feat(ffa): update rate limit system
    Signed-off-by: ThisisYoYoDev <yoel.edery@epitech.eu>

--- backend/src/sendfile.py
+++ backend/src/sendfile.py
@@ -1,46 +1,30 @@
 import requests
 from time import sleep
 from fastapi.exceptions import HTTPException
-from itertools import cycle
-from .constants import WEBHOOK_DICT, MAX_RETRIES, WAIT_TIME_INITIAL, RATE_LIMITS
+from .constants import WEBHOOK, WEBHOOK_DICT
 
 def sendfile(buffer):
-    retries = 3
     session = requests.Session()
-
-    webhook_cycle = cycle(WEBHOOK_DICT)
 
     while True:
         try:
-            webhook = next(webhook_cycle)
-            remaining = RATE_LIMITS[webhook]["x-ratelimit-remaining"]
-
-            print(f"Remaining rate limit for {webhook}: {remaining}")
+            webhook = next(WEBHOOK)
+            remaining = WEBHOOK_DICT[webhook]["x-ratelimit-remaining"]
+            print(f"Remaining rate limit for {webhook.split('/')[-1]}: {remaining}")
 
             if remaining == None or int(remaining) > 0:
-                headers = WEBHOOK_DICT[webhook]
-                response = session.post(webhook, files={"file[0]": ("rip", buffer)}, headers=headers)
-                remaining = response.headers.get("x-ratelimit-remaining")
+                response = session.post(webhook, files={"file[0]": ("rip", buffer)})
+                [WEBHOOK_DICT[webhook].update({key: response.headers.get(key)}) for key in WEBHOOK_DICT[webhook].keys()]
 
-                RATE_LIMITS[webhook]["x-ratelimit-remaining"] = remaining
-
-                if response.status_code == 429:
-                    retry_after = response.json().get("retry_after", WAIT_TIME_INITIAL)
-                    print(f"Rate limited. Retrying in {retry_after} seconds...")
-                    sleep(retry_after)
-                    retries += 1
-                else:
-                    response.raise_for_status()
-                    attachments = response.json().get("attachments", [])
-                    urls = []
-                    for attachment in attachments:
-                        url = attachment["url"].split("attachments/")[1].split("/")[:2]
-                        urls.append(f"{url[0]}/{url[1]}")
-                    return urls
+                response.raise_for_status()
+                attachments = response.json().get("attachments", [])
+                urls = []
+                for attachment in attachments:
+                    url = attachment["url"].split("attachments/")[1].split("/")[:2]
+                    urls.append(f"{url[0]}/{url[1]}")
+                return urls
             else:
                 print("Rate limit exceeded. Trying the next webhook.")
-                
-            print(f"Retrying... ({retries}/{MAX_RETRIES})")
         except requests.exceptions.RequestException as error:
             print(error)
-            return HTTPException(status_code=500, detail=f"Failed to send file. ({retries}/{MAX_RETRIES})")
+            return HTTPException(status_code=500, detail=f"Error: {error}")
